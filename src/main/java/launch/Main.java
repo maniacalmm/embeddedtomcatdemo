a302284ccb0ddb1fb6449d44ad147ccdb5e2bbf5
Tang <out_tang_look@outlook.com>
2018-10-30 10:28:08 +0900

    initial commit

---
+++ src/main/java/launch/Main.java
@@ -0,0 +1,49 @@
+package launch;
+
+import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
+
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        String webappDirLocation = "src/main/webapp";
+        Tomcat tomcat = new Tomcat();
+
+        // the port that we should run on can be set into an env var
+        // look for that var and default to 8080
+        String webPort = System.getenv("PORT");
+        if (webPort == null || webPort.isEmpty())
+            webPort = "7777";
+
+        tomcat.setPort(Integer.valueOf(webPort));
+
+        StandardContext ctx =
+                (StandardContext) tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+
+        System.out.println("configuring app with basedir: " + new File("./" + webappDirLocation).getAbsolutePath());
+
+        // Declare an alternative location for your "WEB-INF/classes" dir
+        // Servlet 3.0 annotation will work
+
+        File additionWebInfClasses = new File("target/classes");
+        WebResourceRoot resources = new StandardRoot(ctx);
+        resources.addPreResources(
+                new DirResourceSet(
+                        resources,
+                        "/WEB-INF/classes",
+                        additionWebInfClasses.getAbsolutePath(),
+                        "/"
+                        )
+        );
+
+        ctx.setResources(resources);
+
+        tomcat.start();
+        tomcat.getServer().await();
+
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+    <body>
+        <h2>This is main page</h2>
+    </body>
+</html>
